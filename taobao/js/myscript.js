86c40991863fb0892c8040316f64ba5d8d8bed35
Maoqi <eita.xbx@gmail.com>
2018-02-03 14:26:25 +0800

    update

--- taobao/js/myscript.js
+++ taobao/js/myscript.js
@@ -3,15 +3,9 @@
 // .index-mod__order-container___1ur4- js-order-container	已买到的宝贝List
 //     .bought-wrapper-mod__trade-order___2lrzV	通过属性'data-id'得到订单号
 //     .bought-wrapper-mod__create-time___yNWVS	订单日期(yyyy-MM-dd)
-//     .text-mod__link___2PH1j		交易状态('')
+//     .text-mod__link___2PH1j		交易状态(除去'关闭，成功')
 
-var str="";
-// $("div .bought-wrapper-mod__trade-order___2lrzV").each(function () {
-//
-//     str+=$(this).attr("data-id")+"\r";
-// })
-// str = $(".icon-mod__icon___20QnX.tm-iconfont.icon-message.icon-mod__icon___s6qvQ.icon-mod__message___QtoVE").first().css();
-
+$(".bought-wrapper-mod__trade-order___2lrzV")
 
 
 
